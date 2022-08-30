25710d5b5fb73dd328a2c46f8e5dd862b5d8af4c
havana723 <71638185+havana723@users.noreply.github.com>
2022-08-30 18:13:02 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import "./App.css";
+import Gameboard from "./Gameboard";
+
+const Background = styled.div`
+  height: 100%;
+  width: 100%;
+  background: black;
+  object-fit: contain;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Background>
+      <Gameboard />
+    </Background>
   );
 }
 
